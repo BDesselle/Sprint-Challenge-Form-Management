35f0cde1dee46c3b3ac03697519f99054a6d72e8
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-09 17:00:34 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import RegForm from "./components/RegForm";
+import Navigation from "./components/Navigation";
+import DataList from "./components/DataList";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">testing...</div>;
+  // eslint-disable-next-line
+  const [token, setToken] = useLocalStorage("token");
+  return (
+    <div data-testid="app-container" className="App">
+      <Navigation />
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={props => <RegForm setToken={setToken} />}
+        />
+        <Route exact path="/data" component={DataList} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
