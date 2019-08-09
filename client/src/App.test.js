424b7172a0a852602473df25589882dfb1e4e171
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-09 17:00:37 -0500

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as tl from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import App from "./App";
+/* import DataList from "./components/DataList";
+import Navigation from "./components/Navigation";
+import RegForm from "./components/RegForm"; */
 
-it('renders without crashing', () => {
+/* it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+}); */
+
+describe("App Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("Name", () => {
+  it("Does", () => {
+    /* test */
+  });
 });
